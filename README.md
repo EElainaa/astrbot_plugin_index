0a8aa78f6899c4cad18a7cf7c443be2b5a2d4765
EElainaya <eelainaya@outlook.com>
2026-04-16 22:45:43 +0800

    初步完成功能

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
-# astrbot-plugin-helloworld
+# astrbot-plugin-index
 
-AstrBot 插件模板 / A template plugin for AstrBot plugin feature
-
-> [!NOTE]
-> This repo is just a template of [AstrBot](https://github.com/AstrBotDevs/AstrBot) Plugin.
-> 
-> [AstrBot](https://github.com/AstrBotDevs/AstrBot) is an agentic assistant for both personal and group conversations. It can be deployed across dozens of mainstream instant messaging platforms, including QQ, Telegram, Feishu, DingTalk, Slack, LINE, Discord, Matrix, etc. In addition, it provides a reliable and extensible conversational AI infrastructure for individuals, developers, and teams. Whether you need a personal AI companion, an intelligent customer support agent, an automation assistant, or an enterprise knowledge base, AstrBot enables you to quickly build AI applications directly within your existing messaging workflows.
+指令之意
+每日获取不同的指令
 
 # Supports
 
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+from pathlib import Path
+from datetime import datetime, date
+import instruction
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+@register("astrbot_plugin_instruction", "指令之意", "获取每日的指令", "1.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
@@ -10,15 +13,27 @@ class MyPlugin(Star):
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+    @filter.command("instruction")
+    async def get_instruction(self, event: AstrMessageEvent):
+        """获取指令图片并发送"""
         user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        user_id = event.get_sender_id()
+        user_image_path = f"data/plugin_data/astrbot_plugin_instruction/image/{user_id}.png"
+        if is_created_today(user_image_path):
+            yield event.image_result(user_image_path)
+        else:
+            instruction.create_instruction(username=user_name,output=user_image_path)
+            yield event.image_result(user_image_path)
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+
+def is_created_today(path: str) -> bool:
+    """判断文件是否是今天创建的"""
+    file_path = Path(path)
+    if not file_path.exists():
+        return False
+    
+    ctime_timestamp = file_path.stat().st_birthtime 
+    created_date = datetime.fromtimestamp(ctime_timestamp).date()
+    return created_date == date.today()
